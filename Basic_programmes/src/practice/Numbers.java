d16d4a03021d7cb0f52c9a5432f32bf21bca6110
borasrilakshmi <52118638+borasrilakshmi@users.noreply.github.com>
2019-07-02 21:21:23 +0530

    Update Numbers.java

--- Basic_programmes/src/practice/Numbers.java
+++ Basic_programmes/src/practice/Numbers.java
@@ -6,7 +6,7 @@ static	int n1=0,n2=1,n3=0,sum=1;
 		int n1=0,n2=1,n3; 
 		System.out.print(n1+" "+n2+" ");
 		for(int i=2;i<count;i++) {
-			n3=n1+n2; 
+			n3=n1+n2; /// Numbers adding 
 			n1=n2;n2=n3;
 			System.out.print(n3+" ");
 		}
